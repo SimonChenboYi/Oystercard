14dfc77b7fd3cf0962059d886aee8f75d76f75da
Chenbo Yi <yichenbo@outlook.com>
2019-04-08 16:28:44 +0100

    Add top_up function

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,13 @@
-require "oystercard"
+require 'oystercard'
 
 describe Oystercard do
-	it "initialize balance to 0" do
-		expect(subject.balance).to eq 0
-	end
+  it 'initialize balance to 0' do
+    expect(subject.balance).to eq 0
+  end
 
+  describe '#top_up' do
+    it 'top up the balance' do
+      expect { subject.top_up(1) }.to change { subject.balance }.by 1
+    end
+  end
 end
